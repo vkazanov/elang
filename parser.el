6c9751d1491c9c552ae54e1e9a4b8f9e6edd5a87
Vladimir Kazanov <vkazanov@inbox.ru>
2015-05-22 18:08:34 +0300

    basic parser structure

---
+++ parser.el
@@ -0,0 +1,29 @@
+;; -*- lexical-binding: t -*-
+;;
+;; A simple RD parser for a Python-like language. Directly outputs an AST.
+
+(require 'tokenizer)
+
+(eval-when-compile (require 'names))
+
+(define-namespace tokenizer-
+
+;;; Parser
+;;; ------
+
+(defvar token-stream
+  nil)
+
+(defun parse (tokens)
+  (setq token-stream tokens))
+
+(defun parse-stmt ()
+  )
+
+(defun parse-simple-stmt ()
+  )
+
+(defun parse-compound-stmt ()
+  )
+
+)

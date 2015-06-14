a49293b961ebf5c7f804113075d89719eae2aa51
Vladimir Kazanov <vkazanov@inbox.ru>
2015-06-14 17:23:58 +0300

    compiling basic expressions with constants

--- src/compiler.el
+++ src/compiler.el
@@ -8,6 +8,24 @@
 
 (define-namespace compiler-
 
+(defun compile-to-lapcode (parse-tree)
+  (let (codes
+        constants)
+    (cl-labels
+        ((compile (tree)
+                  (cond
+                   ((or (symbolp tree)
+                        (numberp tree))
+                    (push `(byte-constant . ,(length constants)) codes)
+                    (push tree constants))
+                   ((listp tree)
+                    (mapc #'compile tree)
+                    (push `(byte-call . ,(1- (length tree))) codes))
+                   (t (error "not implemented") ))))
+      (compile parse-tree)
+      (push '(byte-return) codes)
+      (values (reverse codes) (vconcat (reverse constants))))))
+
 ) ;;; end of compiler- namespace
 
 (provide 'compiler)
--- test/compiler-test.el
+++ test/compiler-test.el
@@ -1,11 +1,41 @@
 ;; -*- lexical-binding: t -*-
 ;;
-;;; Tests for compiler.el compiling to Emacs Lisp.
+;;; Tests for compiler.el
 
 (require 'compiler)
 (require 'parser)
 (require 'tokenizer)
 
 (ert-deftest compiler-test-expr ()
+  (with-parsed "1"
+    (dbind (codes constants) (compiler-compile-to-lapcode parse-tree)
+      (should (equal '((byte-constant . 0)
+                       (byte-return))
+                     codes))
+      (should (equal [1]
+                     constants))))
+
   (with-parsed "1 + 2"
-    (message "PARSE TREE: %s" parse-tree)))
+    (dbind (codes constants) (compiler-compile-to-lapcode parse-tree)
+      (should (equal '((byte-constant . 0)
+                       (byte-constant . 1)
+                       (byte-constant . 2)
+                       (byte-call . 2)
+                       (byte-return))
+                     codes))
+      (should (equal [+ 1 2]
+                     constants))))
+
+  (with-parsed "1 + 2 + 3"
+    (dbind (codes constants) (compiler-compile-to-lapcode parse-tree)
+      (should (equal '((byte-constant . 0)
+                       (byte-constant . 1)
+                       (byte-constant . 2)
+                       (byte-constant . 3)
+                       (byte-call . 2)
+                       (byte-constant . 4)
+                       (byte-call . 2)
+                       (byte-return))
+                     codes))
+      (should (equal [+ + 1 2 3]
+                     constants)))))
